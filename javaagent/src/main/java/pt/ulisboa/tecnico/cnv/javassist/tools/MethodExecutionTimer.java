55155264ed711754f3341fa27ad8982d80da553c
TheCodingWombat <richardkoetschruyter@gmail.com>
2024-05-16 18:11:10 +0100

    Sends time back through http with header methodExecutionTimeNs

--- javaagent/src/main/java/pt/ulisboa/tecnico/cnv/javassist/tools/MethodExecutionTimer.java
+++ javaagent/src/main/java/pt/ulisboa/tecnico/cnv/javassist/tools/MethodExecutionTimer.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 import javassist.CtBehavior;
 import javassist.CtClass;
+import javassist.CannotCompileException;
+import javassist.CtMethod;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 
 public class MethodExecutionTimer extends CodeDumper {
 
@@ -18,7 +22,28 @@ public class MethodExecutionTimer extends CodeDumper {
         behavior.insertBefore("startTime = System.nanoTime();");
         StringBuilder builder = new StringBuilder();
         behavior.addLocalVariable("endTime", CtClass.longType);
+        behavior.addLocalVariable("endTime2", CtClass.longType);
         behavior.addLocalVariable("opTime", CtClass.longType);
+        behavior.addLocalVariable("opTime2", CtClass.longType); // this does not include the time for sending the http stuff
+
+
+        // Return metrics through http:
+        if (behavior instanceof CtMethod && behavior.getName().equals("handle")) {
+            behavior.instrument(new ExprEditor() {
+                public void edit(MethodCall m) throws CannotCompileException {
+                    if (m.getMethodName().equals("sendResponseHeaders")) {
+                        m.replace("{"
+                            + " System.out.println(\"yes here\");\n"
+                            + " endTime2 = System.nanoTime();\n"
+                            + " opTime2 = endTime2-startTime;\n"
+                            + "    $0.getResponseHeaders().add(\"methodExecutionTimeNs\", \"\" + opTime2);\n"
+                            + "    $proceed($$);"
+                            + "}");
+                    }
+                }
+            });
+        }
+
         builder.append("endTime = System.nanoTime();");
         builder.append("opTime = endTime-startTime;");
         builder.append(String.format("System.out.println(\"[%s] %s method call completed in: \" + opTime + \" ns!\");",
