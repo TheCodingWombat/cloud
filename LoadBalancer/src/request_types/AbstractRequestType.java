b188f503f1de5e09f1b19f76e872ff0dcc7fd658
blackPanther <herrt@dhcp-wlce193128.zim.uni-duisburg-essen.de>
2024-05-16 16:53:40 +0100

    feat(AbstractRequest) add FactoryMethod

--- LoadBalancer/src/request_types/AbstractRequestType.java
+++ LoadBalancer/src/request_types/AbstractRequestType.java
@@ -1,5 +1,19 @@
 package request_types;
 
+import java.net.URI;
+
 public abstract class AbstractRequestType {
 
+	public static AbstractRequestType ofResouce(URI requestedUri) {
+		if (requestedUri.equals("/blurimage")) {
+			return new BlurImageRequest();
+		} else if (requestedUri.equals("/enhanceimage")) {
+			return new EnhanceImageRequest();
+		} else if (requestedUri.equals("/raytracer")) {
+			return new RayTracerRequest();
+		} else {
+			//maybe add error handling here
+			return null;
+		}
+	}
 }
