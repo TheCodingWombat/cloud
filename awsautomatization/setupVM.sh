7d580a37f5edcee832c16721b122575bcfeafeda
Teodor Chakarov <tedochakarov@gmial.com>
2024-05-20 21:59:40 +0100

    Commit aws scripts

--- awsautomatization/setupVM.sh
+++ awsautomatization/setupVM.sh
@@ -15,11 +15,11 @@ sleep 10;
 sudo yum install -y apache-maven;
 cd /home/ec2-user/cloud/cnv24-g22-master;
 mvn clean package;
-nohup java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer > outputs/webserver.log 2>&1;
+#nohup java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer > outputs/webserver.log 2>&1;
 EOF
 )
 
-cmd=$(cat "nohup java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer > outputs/webserver.log 2>&1;")
-ssh -o StrictHostKeyChecking=no -i "config/ssh.pem" ec2-user@$(cat outputs/instance.dns) $cmd > outputs/ssh_output.log 2>&1
+#cmd=$(cat "nohup java -cp webserver/target/webserver-1.0.0-SNAPSHOT-jar-with-dependencies.jar pt.ulisboa.tecnico.cnv.webserver.WebServer > outputs/webserver.log 2>&1;")
+ssh -o StrictHostKeyChecking=no -i "~/.aws/ssh.pem" ec2-user@$(cat outputs/instance.dns) $cmd > outputs/ssh_output.log 2>&1
 
 
