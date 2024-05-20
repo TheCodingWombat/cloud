433467786dc4d839ec3e6678730a036d73f9100b
TheCodingWombat <richardkoetschruyter@gmail.com>
2024-05-20 12:53:04 +0100

    Extract metrics from http headers and inser them into metricStorageSystem.

--- LoadBalancer/src/metric_storage_system/MetricStorageSystem.java
+++ LoadBalancer/src/metric_storage_system/MetricStorageSystem.java
@@ -11,11 +11,16 @@ import request_types.BlurImageRequest;
  * This class will hold the data that were transfered from the instrumented classes and will return the calculated results for a new request.
  */
 public class MetricStorageSystem {
+	// TODO: WHy is it an arraylist of a map? Why not just a hashmap? Current implementation will be a list of single  element maps
+	static ArrayList<HashMap<BlurImageRequest, RequestMetrics>> blureImageMetrics = new ArrayList<HashMap<BlurImageRequest,RequestMetrics>>(); 
 	
-	ArrayList<HashMap<BlurImageRequest, RequestMetrics>> blureImageMetrics = new ArrayList<HashMap<BlurImageRequest,RequestMetrics>>(); 
-	
-	public void storeMetric(AbstractRequestType requestType, RequestMetrics metric) {
-		
+	// TODO: Shouldn't we just have a class for this for each metric type?
+	public static void storeMetric(AbstractRequestType requestType, RequestMetrics metric) {
+		if(requestType instanceof BlurImageRequest) {
+			HashMap<BlurImageRequest, RequestMetrics> map = new HashMap<BlurImageRequest, RequestMetrics>();
+			map.put((BlurImageRequest) requestType, metric);
+			blureImageMetrics.add(map);
+		}
 	}
 
 	public static RequestEstimation calculateEstimation(AbstractRequestType requestType) {
