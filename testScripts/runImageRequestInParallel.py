5b67425bba38009c5f989a7d066156bade9629c7
blackPanther <herrt@dhcp-wlce193128.zim.uni-duisburg-essen.de>
2024-05-31 15:08:08 +0100

    updatetestcases

--- testScripts/runImageRequestInParallel.py
+++ testScripts/runImageRequestInParallel.py
@@ -1,6 +1,7 @@
 import os
 import random
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 def send_request(url, payload):
     try:
@@ -22,30 +23,38 @@ def load_file_content(file_path):
         print("Fehler beim Laden der Datei:", str(e))
         return None
 
-def send_requests(url, directory_path, num_requests):
- 
+def send_requests(url, url1, directory_path, num_requests, max_workers=10):
     files = os.listdir(directory_path)
-    
- 
 
-    for i in range(num_requests):
-        print("Anfrage", i+1)
+    def task():
         # Zufällige Auswahl einer Datei aus der Liste
         file_name = random.choice(files)
-        file_path = directory_path + file_name
+        file_path = os.path.join(directory_path, file_name)
         print(file_path)
 
         # Laden des Inhalts aus der ausgewählten Datei
         payload = load_file_content(file_path)
 
         if payload is not None:
-            send_request(url, payload)
+            randomInt = random.randint(0, 1)
+            if randomInt == 0 :
+                send_request(url, payload)
+            else :
+                send_request(url1, payload)
         else:
             print("Dateiinhalt null.")
 
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = [executor.submit(task) for _ in range(num_requests)]
+        for future in futures:
+            try:
+                future.result()  # Warten auf das Ergebnis, um Ausnahmen zu behandeln
+            except Exception as e:
+                print("Fehler bei der Verarbeitung einer Anfrage:", str(e))
 
+url = "http://16.16.179.113:8001/enhanceimage"
+url1 = "http://16.16.179.113:8001/blurimage"
 
-url = "http://16.16.179.113:8001/blurimage"
 directory_path = "C:\\Users\\herrt\\git\\cloud\\testScripts\\base64_encoded_pictures\\"
 num_requests = 100
-send_requests(url, directory_path, num_requests)
+send_requests(url, url1 , directory_path, num_requests)
